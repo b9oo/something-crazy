7528372d486166cee7061735c6ac861d7977552a
b9oo <rishsirc@proton.me>
2026-06-19 11:09:24 -0500

    Refactor SuperFX functions with improved comments

--- main.ts
+++ main.ts
@@ -1,38 +1,56 @@
+/**
+ * SuperFX Extension
+ */
+
+//% color=#AA00FF icon="\uf0e7"
 namespace SuperFX {
-    export function shakeScreen(duration: number) {
-        scene.cameraShake(4, duration)
+
+    /**
+     * Shake the screen
+     */
+    //% block="shake screen for $ms ms"
+    //% ms.defl=500
+    export function shakeScreen(ms: number) {
+        scene.cameraShake(4, ms)
     }
 
-    export function showDamage(
-        target: Sprite,
-        amount: number
-    ) {
+    /**
+     * Show damage text above a sprite
+     */
+    //% block="show damage $amount on $sprite"
+    export function showDamage(sprite: Sprite, amount: number) {
         let txt = textsprite.create("-" + amount)
-        txt.setPosition(target.x, target.y - 10)
-
+        txt.setPosition(sprite.x, sprite.y - 10)
         txt.vy = -20
 
-        timer.after(1000, function () {
-            txt.destroy()
-        })
+        pause(1000)
+        txt.destroy()
     }
 
+    /**
+     * Create an explosion effect
+     */
+    //% block="explosion at x $x y $y"
     export function explosion(x: number, y: number) {
-        effects.fire.startScreenEffect(500)
-        sprites.createProjectileFromSide(0, 0, img`
+        let p = sprites.create(img`
             . 2 2 .
             2 4 4 2
             2 4 4 2
             . 2 2 .
-        `).setPosition(x, y)
+        `)
+
+        p.setPosition(x, y)
+        p.startEffect(effects.fire, 500)
+
+        pause(500)
+        p.destroy()
+    }
+
+    /**
+     * Rainbow sprite effect
+     */
+    //% block="rainbow effect on $sprite"
+    export function rainbow(sprite: Sprite) {
+        sprite.startEffect(effects.trail)
     }
 }
-//% block="shake screen for $ms ms"
-//% ms.defl=500
-export function shakeScreen(ms: number)
-
-//% block="show damage $amount on $sprite"
-export function showDamage(sprite: Sprite, amount: number)
-
-//% block="explosion at x $x y $y"
-export function explosion(x: number, y: number)
